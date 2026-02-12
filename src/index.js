7b44fb25b24db21e0113d39f0e6a8d36800fcfb7
AjaySusanth <ajaysusanth10@gmail.com>
2026-02-12 13:08:26 +0530

    feat: create and get matches endpoints

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import express from "express";
+import {matchRouter} from './routes/matches.js'
+
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+
+app.use('/matches',matchRouter)
 
 app.get("/", (req, res) => {
   res.json({ message: "Server is running" });
